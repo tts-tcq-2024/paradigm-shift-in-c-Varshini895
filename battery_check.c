fd8bec22ddf5e48e036dd745eafa668ca7952abe
Varshini895 <varshinireddynallamilli@gmail.com>
2024-08-02 22:05:45 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -1,13 +1,12 @@
 #include "battery_check.h"
 #include "lang_sup.h"
-
 #define MIN_TEMPERATURE 0.0
 #define MAX_TEMPERATURE 45.0
 #define MIN_SOC 20.0
 #define MAX_SOC 80.0
 #define MAX_CHARGE_RATE 0.8
 
-int isTemperatureOk(float temperature, Language lang) {
+int isTemperatureOk(float temperature, int lang) {
     if (temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
         printf("%s\n", getTemperatureOutOfRangeMessage(lang));
         return 0; // Not okay
@@ -15,7 +14,7 @@ int isTemperatureOk(float temperature, Language lang) {
     return 1; // Okay
 }
 
-int isSocOk(float soc, Language lang) {
+int isSocOk(float soc, int lang) {
     if (soc < MIN_SOC || soc > MAX_SOC) {
         printf("%s\n", getSocOutOfRangeMessage(lang));
         return 0; // Not okay
@@ -23,7 +22,7 @@ int isSocOk(float soc, Language lang) {
     return 1; // Okay
 }
 
-int isChargeRateOk(float chargeRate, Language lang) {
+int isChargeRateOk(float chargeRate, int lang) {
     if (chargeRate > MAX_CHARGE_RATE) {
         printf("%s\n", getChargeRateOutOfRangeMessage(lang));
         return 0; // Not okay
@@ -31,7 +30,7 @@ int isChargeRateOk(float chargeRate, Language lang) {
     return 1; // Okay
 }
 
-int batteryIsOk(float temperature, float soc, float chargeRate, Language lang) {
+int batteryIsOk(float temperature, float soc, float chargeRate, int lang) {
     return isTemperatureOk(temperature, lang) &&
            isSocOk(soc, lang) &&
            isChargeRateOk(chargeRate, lang);
