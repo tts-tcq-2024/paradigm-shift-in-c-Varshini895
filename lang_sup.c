06c4a0b3a53cf4679fa6d32eb3e06ba7347f13d8
Varshini895 <varshinireddynallamilli@gmail.com>
2024-08-02 21:58:12 +0530

    Create lang_sup.c

---
+++ lang_sup.c
@@ -0,0 +1,26 @@
+#include "lang_sup.h"
+
+const char* messages[LANG_COUNT][3] = {
+    [LANG_ENGL] = {
+        "Temperature out of range!",
+        "State of Charge out of range!",
+        "Charge Rate out of range!"
+    },
+    [LANG_GER] = {
+        "Temperatur außerhalb des Bereichs!",
+        "Ladezustand außerhalb des Bereichs!",
+        "Laderate außerhalb des zulässigen Bereichs!"
+    }
+};
+
+const char* getTemperatureOutOfRangeMessage(Language lang) {
+    return messages[lang][0];
+}
+
+const char* getSocOutOfRangeMessage(Language lang) {
+    return messages[lang][1];
+}
+
+const char* getChargeRateOutOfRangeMessage(Language lang) {
+    return messages[lang][2];
+}
