5bb583d5293525b9d0476c9af4f95c9c0ddcc3f6
Venugopal K <venu.shastri@gmail.com>
2024-08-06 11:24:33 +0530

    Update battery_check.c

--- battery_check.c
+++ battery_check.c
@@ -6,17 +6,21 @@
 #define MAX_SOC 80.0
 #define MAX_CHARGE_RATE 0.8
 
-int isTemperatureOk(float temperature, int lang) {
+
+
+int preferedLanguage=0;
+
+int isTemperatureOk(float temperature) {
     if (temperature < MIN_TEMPERATURE || temperature > MAX_TEMPERATURE) {
-        printf("%s\n", getTemperatureOutOfRangeMessage(lang));
+        
         return 0; // Not okay
     }
     return 1; // Okay
 }
 
-int isSocOk(float soc, int lang) {
+int isSocOk(float soc) {
     if (soc < MIN_SOC || soc > MAX_SOC) {
-        printf("%s\n", getSocOutOfRangeMessage(lang));
+        
         return 0; // Not okay
     }
     return 1; // Okay
@@ -24,14 +28,28 @@ int isSocOk(float soc, int lang) {
 
 int isChargeRateOk(float chargeRate, int lang) {
     if (chargeRate > MAX_CHARGE_RATE) {
-        printf("%s\n", getChargeRateOutOfRangeMessage(lang));
+        
         return 0; // Not okay
     }
     return 1; // Okay
 }
 
 int batteryIsOk(float temperature, float soc, float chargeRate, int lang) {
-    return isTemperatureOk(temperature, lang) &&
-           isSocOk(soc, lang) &&
-           isChargeRateOk(chargeRate, lang);
+    int tempOk=isTemperatureOk(temperature);
+    int socOk=isSocOk(soc);
+    int chargeRateOk=isChargeRateOk(chargeRate);
+
+   printErrorMessage( getErrorMessage(tempOk,lang,0));
+    printErrorMessage(getErrorMessage(socOk,lang,1));
+    printErrorMessage(getErrorMessage(chargeRateOk,lang,1));
+    return  tempOk&&
+           socOk &&
+           chargeRateOk(chargeRate, lang);
+}
+char* getErrorMessage(int status, int lang,int parameterIndex){
+return getLangaugaeSpecificMessage(lang,parameterIndex);
+}
+
+void printMessageOnConsole(char* message){
+   printf("%s",message);
 }
